7b03693070d496ffb93641b8debca0cbcc172c5a
bianet13 <bia.macedo13@gmail.com>
2015-09-27 19:18:53 -0300

    init commit

--- To Do List/SecondViewController.swift
+++ To Do List/SecondViewController.swift
@@ -8,16 +8,37 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var itemTextField: UITextField!
+    
+    @IBAction func addItem() {
+        
+        toDoList.append(itemTextField.text!)
+        
+        itemTextField.text = ""
+        
+        NSUserDefaults.standardUserDefaults().setObject(toDoList, forKey: "toDoList")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.itemTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        itemTextField.resignFirstResponder()
+        return true
     }
 
 
